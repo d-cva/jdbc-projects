03e460b6cf84236f1c23f9196e46b9b3572eb717
shivanita <shivanita@10.0.0.23>
2018-07-21 12:06:18 -0700

    First comment

---
+++ StaticSQL/src/com/jdbc/practice/Main.java
@@ -0,0 +1,44 @@
+package com.jdbc.practice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+	private static final String USERNAME= "shiva";
+	private static final String PASSWORD ="password";
+	private static final String CONN_STRING =
+			"jdbc:mysql://localhost/explorecalifornia";
+	public static void main(String[] args) throws SQLException {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+//			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//			rs = stmt.executeQuery("SELECT * FROM states");
+//			rs.last();
+
+			System.out.println("Connected ");
+			
+		} catch (SQLException e) {
+			System.err.println(e);
+		} finally {
+			if(rs != null) {
+				rs.close();
+			}
+			if(stmt != null) {
+				stmt.close();
+			}
+			if(conn != null) {
+				conn.close();
+			}
+		}
+
+	}
+
+}
